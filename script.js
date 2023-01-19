c91d524e403a8213d1f910acb6a587132f249eb5
Luan Carlos <tatalc4e@gmail.com>
2023-01-19 01:10:42 -0300

    ficah exibe dados do formulario

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function submitForm() {
-  var name = document.getElementById("name").value;
-  document.getElementById("name").innerHTML = name;
-  //And so on for the rest of the fields
+function submitForm(event) {
+  event.preventDefault();
+  // pegar os valores dos inputs do formulário e atribuir os valores aos elementos da tabela da ficha
+
+  const name = document.getElementById("name").value;
+  const className = document.getElementById("class").value;
+  const level = document.getElementById("level").value;
+  const race = document.getElementById("race").value;
+  const alignment = document.getElementById("alignment").value;
+  const hitPoints = document.getElementById("hitPoints").value;
+  const abilities = document.getElementById("abilities").value;
+  const equipment = document.getElementById("equipment").value;
+
+  // atribuir os valores aos elementos da tabela da ficha
+  document.getElementById("name-display").innerHTML = name;
+  document.getElementById("class-display").innerHTML = className;
+  document.getElementById("level-display").innerHTML = level;
+  document.getElementById("race-display").innerHTML = race;
+  document.getElementById("alignment-display").innerHTML = alignment;
+  document.getElementById("hitPoints-display").innerHTML = hitPoints;
+  document.getElementById("abilities-display").innerHTML =
+    document.getElementById("abilities").value;
+  document.getElementById("equipment-display").innerHTML = equipment;
 }
